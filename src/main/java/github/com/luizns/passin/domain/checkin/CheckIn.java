47c3e5ff92a925a4fa01b7b1e1a4316fb1ed9c22
Luiz Nascimento <luns.wm@hotmail.com>
2024-04-08 14:11:18 -0300

    feat: add domain class

---
+++ src/main/java/github/com/luizns/passin/domain/checkin/CheckIn.java
@@ -0,0 +1,30 @@
+package github.com.luizns.passin.domain.checkin;
+
+
+import github.com.luizns.passin.domain.attendee.Attendee;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_in")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CheckIn {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id")
+    private Attendee attendee;
+}
